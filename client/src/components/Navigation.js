a4cab2633d0cfe8df5fd2a4d73ab1112bc4d4795
Henry Zhang <hzhang@k2digital.com>
2019-02-25 18:34:42 -0500

    styling

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom";
 
 const Navigation = (props) => {
   return <div className={"navigation"}>
+    <div className="logo"><Link to={"/"}><div className="key"><div className="keycap">P</div></div></Link></div>
     <ul>
-      <li><Link to={"/"}><div className="key"><div className="keycap">P</div></div></Link></li>
       <li><Link to={"/game-room"}>Game Room</Link></li>
       <li><Link to={"/leaderboard"}>Leaderboard</Link></li>
     </ul>
