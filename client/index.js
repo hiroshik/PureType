f128e7368039c72b968116dbdfde787382c3dfe8
Henry Zhang <hzhang@k2digital.com>
2019-03-11 17:31:52 -0400

    Add express server for front end

---
+++ client/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = 3999;
+
+app.use(express.static(path.join(__dirname, '/build')))
+app.get(['/','/*'], function(req, res) {
+  console.log('Routing...');
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+app.listen(port);
+console.log('Server running at: http://localhost:' + port);
